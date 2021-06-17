084739fd43bc2ea86f590c3c8d53218d1baa6e8d
Chebanro2807 <Chebanro2807@ukr.net>
2021-06-17 20:10:57 +0300

    create random massives with pictures for barabans

--- src/app.js
+++ src/app.js
@@ -14,27 +14,25 @@ class App {
 
         this.loader = PIXI.Loader.shared;
         this.StartButton = new Start();
-        this.slots = [new Slot('A'),new Slot('B'),new Slot('C'),new Slot('D'),new Slot('E'),new Slot('F'),new Slot('G'),new Slot('H')];
-        this.weels = [];
+        let slots = [new Slot('A'),new Slot('B'),new Slot('C'),new Slot('D'),new Slot('E'),new Slot('F'),new Slot('G'),new Slot('H')];
+        this.wheels = [];
         for(let i = 0; i < 5; i++) {
-            this.weels.push(new Wheel(i));
+            this.wheels.push(new Wheel(i, this.shuffleArray(slots)));
         }
-        this.slots.forEach(element => {
+        slots.forEach(element => {
             this.loader.add("../src/assets/symbols/" + element._name + ".png");
         });
         this.loader.load(this.setup.bind(this));
     }
+
     setup() {
-        this.weels.forEach(wheel => {
-            this.shuffledSlots = wheel.shuffleArray(this.slots);
-            let picture = this.randomPicture();
-            wheel.draw(this.shuffledSlots[picture].preSprite(this.loader),1,this.app);
-            wheel.draw(this.shuffledSlots[(picture === 0) ? this.slots.length-1 : picture-1].preSprite(this.loader),0,this.app);
-            wheel.draw(this.shuffledSlots[(picture === this.slots.length-1) ? 0 : picture+1].preSprite(this.loader),2,this.app); 
+        this.wheels.forEach(wheel => {
+            wheel.drawBaraban(this.loader, this.app);
         })
     }
-    randomPicture() {
-        return Math.floor(Math.random() * (this.slots.length-1));
+
+    shuffleArray(array) {
+        return array.sort(() => Math.random() - 0.5);
     }
 }
 
--- src/elements/wheel.js
+++ src/elements/wheel.js
@@ -1,6 +1,10 @@
 export default class Wheel {
-    constructor(x) {
+    constructor(x, arr) {
         this.x = x;
+        this.baraban = [];
+        for (let i = 0; i < arr.length; i++) { // clone arr
+            this.baraban[i] = arr[i];
+        }
     }
 
     draw(sprite, position, app) {
@@ -11,7 +15,18 @@ export default class Wheel {
         sprite.height=150;
     }
 
-    shuffleArray(array) {
-        return array.sort(() => Math.random() - 0.5);
+    drawBaraban(loader, app) {
+        let picture = this.randomPicture();
+        this.drawBarabanByPict(picture, loader, app);
+    }
+
+    drawBarabanByPict(picture, loader, app) {
+        this.draw(this.baraban[picture].preSprite(loader), 1, app);
+        this.draw(this.baraban[(picture === 0) ? this.baraban.length-1 : picture-1].preSprite(loader), 0, app);
+        this.draw(this.baraban[(picture === this.baraban.length-1) ? 0 : picture+1].preSprite(loader), 2, app); 
+    }
+
+    randomPicture() {
+        return Math.floor(Math.random() * (this.baraban.length-1));
     }
 }
