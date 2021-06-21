0896f25064a39f23a9dc95aef49db12d7f9b3fe5
Chebanro2807 <Chebanro2807@ukr.net>
2021-06-21 16:56:14 +0300

    add more small wins

--- src/elements/smallwin.js
+++ src/elements/smallwin.js
@@ -2,17 +2,35 @@ export default class SmallWin {
     constructor() {}
     
     static win(wheels, score) {
-            SmallWin.square1win1(wheels, score);
+        SmallWin.rowWin(wheels, score);
+        SmallWin.diagonalWinRight(wheels, score);
+        SmallWin.diagonalWinLeft(wheels, score);
     }
 
-    static square1win1(wheels, score) {
+    static rowWin(wheels, score) {
         let poses = ["top","center", "bottom"];
         for (let i=0; i<3; i++) {
             poses.forEach(pos => {
-                if (wheels[i].pos(pos)._name === wheels[i+1].pos(pos)._name && wheels[i].pos(pos)._name === wheels[i+2].pos(pos)._name) {
+                if (wheels[i].pos(pos).equals(wheels[i+1].pos(pos)) && wheels[i].pos(pos).equals(wheels[i+2].pos(pos))) {
                     score.changeScoreByPrize(111);
                 }
             });
+        }
+    }
+
+    static diagonalWinRight(wheels, score) {
+        for (let i=0; i<3; i++) {
+            if (wheels[i].top().equals(wheels[i+1].center()) && wheels[i].top().equals(wheels[i+2].bottom())) {
+                score.changeScoreByPrize(222);
+            }   
+        }
+    }
+
+    static diagonalWinLeft(wheels, score) {
+        for (let i=0; i<3; i++) {
+            if (wheels[i].bottom().equals(wheels[i+1].center()) && wheels[i].bottom().equals(wheels[i+2].top())) {
+                score.changeScoreByPrize(222);
+            }   
         }
     }
 }
