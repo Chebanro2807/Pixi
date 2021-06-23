907475d9e4fd0542116e42458876708bf67df0a5
Chebanro2807 <Chebanro2807@ukr.net>
2021-06-23 11:18:15 +0300

    hot fix

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ class App {
             }
         }
         slots.forEach(element => {
-            this.loader.add("../src/assets/symbols/" + element._name + ".png");
+            this.loader.add("/src/assets/symbols/" + element._name + ".png");
         });
         this.loader.load(this.setup.bind(this));
         this.app.stage.interactive = true;
